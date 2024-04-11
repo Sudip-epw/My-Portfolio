43e9fba5faac9745ccdbdbcdc4955dc7c4962d20
Sudip Halder <sudipepw@gmail.com>
2024-04-11 20:22:02 +0530

    create react app and added navbar and some styles to it

---
+++ My-Portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import styles from './App.module.css'
+import {Navbar} from './components/navbar/Navbar'
+
+function App() {
+
+  return (
+    <div className={styles.App}>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
